80119de84ec9e8259d718c59ccfe40c68d2a349c
rushikesh8876 <inglerushikesh2@gmail.com>
2025-09-17 19:53:57 +0530

    Added static file

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
+STATICFILES_DIRS=[
+    STATIC_DIR,
+]
 STATICFILES_DIRS=[
 STATIC_DIR,
  ]
